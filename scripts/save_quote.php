0a66a19c1441e653f442c031662b12e2c5ba4ae5
raul93611 <raul93611@gmail.com>
2020-06-24 19:35:28 -0400

    "In process" status bug fixed

--- scripts/save_quote.php
+++ scripts/save_quote.php
@@ -7,8 +7,12 @@ if (isset($_POST['guardar_cambios_cotizacion'])) {
   $eta2 = RepositorioRfqFullFillmentComment::english_format_to_mysql_date($_POST['eta2']);
   $eta3 = RepositorioRfqFullFillmentComment::english_format_to_mysql_date($_POST['eta3']);
   $items = RepositorioItemFullFillment::obtener_items_por_id_rfq(ConnectionFullFillment::get_connection(), $_POST['id_rfq']);
-  $description = mb_substr($items[0]-> obtener_description_project(), 0, 100);
-  RfqFullFillmentPartRepository::save_rfq_fullfillmet_info(ConnectionFullFillment::get_connection(), $_POST['rfq_fullfillment_part_name'], $_POST['business_classification'], $description, $po_date, $eta1, $eta2, $eta3, $_POST['comment_consolidate_others'], $_POST['consolidate_others'], $_POST['id_rfq_fullfillment_part']);
+  if(!empty($items)){
+    $description = mb_substr($items[0]-> obtener_description_project(), 0, 100);
+  }else{
+    $description = '';
+  }
+  RfqFullFillmentPartRepository::save_rfq_fullfillmet_info(ConnectionFullFillment::get_connection(), $_POST['rfq_fullfillment_part_name'], $_POST['business_classification'], $description, $po_date, $eta1, $eta2, $eta3, '', '', $_POST['id_rfq_fullfillment_part']);
   RepositorioRfqFullFillment::actualizar_rfq_2(ConnectionFullFillment::get_connection(), htmlspecialchars($_POST['address']), htmlspecialchars($_POST['ship_to']), $_POST['contract_number'], $_POST['total_cost'], $_POST['id_rfq']);
   if(isset($_POST['invoice']) && $_POST['invoice'] == 'invoice'){
     RfqFullFillmentPartRepository::set_status_invoice(ConnectionFullFillment::get_connection(), $_POST['id_rfq']);
